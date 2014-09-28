55727fbed5acc068fc1ddcf659e69c8e306cbcf5
Vgr E.Barry <vgr255@live.ca>
2014-09-28 14:36:59 -0400

    Fix log.multiple and log.help passing a double tuple

--- tools/logger.py
+++ tools/logger.py
@@ -43,6 +43,8 @@ def logger(*output, logtype="", type="normal", display=True, write=True, splitte
             logger(output, logtype=logtype, type=type, display=False, write=write) # display is false because it already displayed anyway
 
 def multiple(*output, types=[], display=True, write=True, splitter=" "):
+    output = list(output)
+    output = splitter.join(output)
     if "all" in types:
         if var.LOG_EVERYTHING or var.DEV_LOG:
             logger(output, type="all", display=display, write=write, splitter=splitter)
@@ -62,4 +64,6 @@ def multiple(*output, types=[], display=True, write=True, splitter=" "):
         logger(output, display=display, write=write, splitter=splitter)
 
 def help(*output, type="help", write=False, display=True, splitter=" "):
+    output = list(output)
+    output = splitter.join(output)
     logger(output, type=type, write=write, display=display, splitter=splitter)
