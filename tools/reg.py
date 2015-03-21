ad923a995016de4a36b85e7065e833f93020ac86
Vgr E.Barry <vgr255@live.ca>
2015-03-21 14:16:08 -0400

    Update the reg module

--- tools/reg.py
+++ tools/reg.py
@@ -1,14 +1,7 @@
 ﻿from tools import variables as var
 import os
 
-if var.ON_WINDOWS:
-    import winreg
-
-if var.GAME_VERSION == 1999: # No game installed, create new reg entry
-    set_new()
-
-if var.GAME_VERSION in (2012, 2013):
-    change()
+import winreg
 
 def add(drive=None, app=None):
     if app is None:
@@ -26,15 +19,22 @@ def add(drive=None, app=None):
     write("DataPath", "{0}Data\\\\".format(app))
     write("MoviePath", "{0}movies\\\\".format(app))
     write("DriverPath", "{0}ff7_opengl.fgd".format(app))
-    write("FullInstall", "00000001", 4)
+    write("FullInstall", 0x00000001, 4)
     write("DriverPath", "{0}ff7_opengl.fgd".format(app), 1, 1)
-    write("Driver", "00000003", 4, 1)
-    write("Mode", "00000002", 4, 1)
+    write("Driver", 0x00000003, 4, 1)
+    write("Mode", 0x00000002, 4, 1)
 
     write("{0}ff7.exe".format(var.FFVII_PATH), "RUNASADMIN", 1, "Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers", 1)
     write("{0}FF7Config.exe".format(var.FFVII_PATH), "RUNASADMIN", 1, "Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers", 1)
 
-    
+def raw(key, value, path, type=1, opener=2, create=None):
+    print(key, value, path)
+    reg = winreg.OpenKey(0xFFFFFFFF80000000+opener, path, 0, 0x20006)
+    if create:
+        winreg.CreateKey(reg, create)
+    if key and value:
+        winreg.SetValueEx(reg, key, 0, type, value)
+    reg.Close()
 
 def write(key=None, value=None, type=1, path=0, opener=2, create=False): # There are no integrity checks
     # Possible types:
@@ -63,7 +63,7 @@ def write(key=None, value=None, type=1, path=0, opener=2, create=False): # There
 
     path_ints = (var.REG_ENTRY, var.REG_GRAPH, var.REG_SOUND, var.REG_MIDI)
 
-    if not (key and value and create):
+    if not (key or value or create):
         return # Not allowed
 
     if isinstance(path, int) or path.isdigit():
@@ -72,18 +72,20 @@ def write(key=None, value=None, type=1, path=0, opener=2, create=False): # There
     reg = winreg.OpenKey(0xFFFFFFFF80000000+opener, path, 0, 0x20006) # Do not alter these numbers
     if create:
         winreg.CreateKey(reg, path)
-    else:
-        winreg.SetValueEx(reg, key, 0, type, value)
+    winreg.SetValueEx(reg, key, 0, type, value)
+    reg.Close()
 
 def get_key(value, path=0, opener=2):
 
-    path_ints = {0: var.REG_ENTRY, 1: var.REG_GRAPH, 2: var.REG_SOUND, 3: var.REG_MIDI}
+    path_ints = (var.REG_ENTRY, var.REG_GRAPH, var.REG_SOUND, var.REG_MIDI)
 
     if isinstance(path, int) or path.isdigit():
         path = path_ints[int(path)]
     try:
         entry = winreg.OpenKey(0xFFFFFFFF80000000+opener, path)
-        return winreg.QueryValueEx(entry, value)
+        ret = winreg.QueryValueEx(entry, value)
+        entry.Close()
+        return ret
     except OSError:
         pass
 
@@ -100,23 +102,29 @@ def set_new(): # Create a new registry entry
     write("AppPath", "")
     write("DataPath", "")
     write("MoviePath", "")
-    write("DiskNo", "00000000", 4)
-    write("FullInstall", "00000001", 4)
-    write("SSI_DEBUG", "53,48,4f,57,4d,45,54,48,45,41,50,50,4c,4f,47,00", 3)
+    write("DiskNo", 0x00000000, 4)
+    write("FullInstall", 0x00000001, 4)
+    write("SSI_DEBUG", b"53,48,4f,57,4d,45,54,48,45,41,50,50,4c,4f,47,00", 3)
     write("DriverPath", "")
     write(path=1, create=True)
-    write("DD_GUID", "00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00", 3, 1)
+    write("DD_GUID", b"00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00", 3, 1)
     write("DriverPath", "", 1, 1)
-    write("Driver", "00000003", 4, 1)
-    write("Mode", "00000002", 4, 1)
-    write("Options", "00000000", 4, 1)
+    write("Driver", 0x00000003, 4, 1)
+    write("Mode", 0x00000002, 4, 1)
+    write("Options", 0x00000000, 4, 1)
     write(path=3, create=True)
-    write("MIDI_DeviceID", "ffffffff", 4, 3)
+    write("MIDI_DeviceID", 0xffffffff, 4, 3)
     write("MIDI_data", "GENERAL_MIDI", 1, 3)
-    write("MusicVolume", "00000064", 4, 3)
-    write("Options", "00000001", 4, 3)
+    write("MusicVolume", 0x00000064, 4, 3)
+    write("Options", 0x00000001, 4, 3)
     write(path=2, create=True)
-    write("Sound_GUID", "00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00", 3, 2)
-    write("Options", "00000000", 4, 2)
-    write("SFXVolume", "00000064", 4, 2)
+    write("Sound_GUID", b"00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00", 3, 2)
+    write("Options", 0x00000000, 4, 2)
+    write("SFXVolume", 0x00000064, 4, 2)
     var.GAME_VERSION = 1998
+
+if var.GAME_VERSION in (1999, None): # No game installed, create new reg entry
+    set_new()
+
+if var.GAME_VERSION in (2012, 2013):
+    change()
